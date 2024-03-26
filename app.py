399004cd31cef60aea45f3abadcae91c50b5334f
simeon <junsimeonanunciado@gmail.com>
2024-03-26 17:11:23 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,92 @@
+# age = 20
+# price = 19.95
+# first_name = "test"
+# is_online = True
+# print(age)
+
+# name = input("What is your name? ")
+# print("Hello "+ name)
+
+
+# birth_year = input("enter your birth year:")
+# age = 2024 - int(birth_year)
+# print(age)
+
+
+# basic calculator
+# first_num = float(input("Enter First #"))
+# second_num = float(input("Enter Second #"))
+# sum = str(first_num+second_num)
+
+# print("sum of two numbers is " + sum)
+
+#OBJECT IN PYTHON
+# course = "Python for Beginners"
+# course.replace("for","4")
+# print("Python" in course)
+
+
+#ARTIHMETIC OPERATION
+
+# if else statement
+# temperature = 5
+# if temperature > 30 :
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif temperature > 20:
+#     print("It's a nice day")
+# elif temperature > 10:
+#     print("bit cold")
+# else :
+#     print("It's cold")
+# print("Done")
+
+
+#EXCERCISE
+
+# weight = float(input("Weight:"))
+# unit = input("(K)g or (L)bs:")
+# result = 0
+
+# if unit.upper() == "L": # 2.205
+#     result = round(weight * 2.205)
+# elif unit.upper() == "K": # * 1
+#     result = int(weight * 1)
+# print("Your weight in " + unit.upper() + " is " + str(result) )
+
+
+# WHILE LOOP
+# i = 1
+# while i <= 10:
+#     print(i * "*")
+#     i+=1
+
+
+#LIST
+names = ["simeon","mary","keani"]
+#print(names[-1])
+numbers = [1,2,3,4,5,6,7,8]
+# numbers.append(9)
+# numbers.insert(0,-1)
+# numbers.remove(3)
+# numbers.clear()
+# print( 10 in numbers)
+# print( len( numbers ) )
+
+
+#FOR LOOP
+# for i in numbers:
+#     print(i)
+# i = 0
+# while i <= len(numbers):
+#     print(numbers[i])
+#     i+=1
+
+# print(3000/10)
+
+#range
+numbers = range(5,10)
+for i in numbers:
+    print(i)
+    i+=1
+# print(numbers)
